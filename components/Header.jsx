230c5a89af38a7a7a68f3f38c558f437652a1286
Yuvraj Singh Raghuwanshi <y.s.raghuwanshi03@gmail.com>
2023-05-05 11:53:53 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
                 </Link>
             </div>
             <div className="flex">
-            <div className="hidden md:flex">
+            <div className="hidden lg:flex">
                 {categories.map((category)=>(
                     <Link key={category.slug} href={`/category/${category.slug}`}>
                         <span className="md:float-right transition duration-500 mt-2 align-middle text-white ml-4 foznt-semibold cursor-pointer hover:text-pink-400 ">
